1a5de85daa7cb07af069ba6220a105042e8fbd8b
Grigory Bogush <g.bogush@leviossa.net>
2019-05-21 12:11:22 +0300

    added pagination config

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,4 @@
+# Configure the number of items on a page at the global level
+# Set global value for pagination number of items per page
+# This can be overridden in a specific model class
+WillPaginate.per_page = 10
